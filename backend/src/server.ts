4e4d3a98fc291fd56ef1481c4bdfe9cfa222d740
stren-12 <smagsf@gmail.com>
2022-12-30 12:02:18 +0300

    Add / route and CI/CD Back-end deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,6 +18,11 @@ app.use(cors(corsOptions))
 products_routes(app)
 users_routes(app)
 orders_routes(app)
+
+// To fix eb health cheacks (if HTTP code is not 200 this will gives you warrning)
+app.get("/",async (req: Request, res: Response) => {
+  res.status(200).json({"health" : "good"})
+})
 app.listen(8080, function () {
   console.log(`starting app on: ${address}`)
 })
